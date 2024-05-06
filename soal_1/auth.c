27c75a260e72b6f5f04152ee98915987a1c7b383
CHiTO <121481079+iryandae@users.noreply.github.com>
2024-05-06 10:08:17 +0700

    Update auth.c

--- soal_1/auth.c
+++ soal_1/auth.c
@@ -8,7 +8,18 @@
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<sys/stat.h>
+#include <time.h>
 
+void timeRecord(char *waktu, char *file){
+	FILE *record=fopen("/home/user/modul_3/soal_1/timeRec.log","a");
+		
+	if(record==NULL){
+		perror("unable to open");
+		exit(EXIT_FAILURE);
+	}
+	fprintf(record, "[%s] %s\n", waktu,file);
+	fclose(record);
+}
 void removeFile(char *str){
 	if(remove(str)==0) printf("%s deleted\n",str);
 }
@@ -54,9 +65,14 @@ void sharedMemory(char *filename){
 
 int main(){
 	
-	char *path="/home/user/modul_3/soal_1/database/";
+	char *path="/home/user/modul_3/soal_1/new_data/";
 	struct dirent *de;
 	struct stat buf;
+	
+	time_t t=time(NULL);
+	struct tm *tm_info=localtime(&t);
+	char time_str[20];
+	strftime(time_str,sizeof(time_str),"%d/%m/%y %H:%M:%S",tm_info);
 	
 	DIR *dir=opendir(path);
 	if(dir==NULL) return 0;
@@ -69,9 +85,10 @@ int main(){
 			char old_path[200],new_path[200];
 			strcpy(old_path,filename);
 			
-			strcpy(new_path,"/home/user/modul_3/soal_1/new_entry/");
+			strcpy(new_path,"/home/user/modul_3/soal_1/microservices/database/");
 			strcat(new_path,de->d_name);
 			moveFile(old_path,new_path);
+			timeRecord(time_str,de->d_name);
 			sharedMemory(new_path);
 		}
 		else{
