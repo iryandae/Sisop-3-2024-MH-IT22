6bec191b2d9047009defe672406c21bd943fe1c5
Combrero <151121570+Combrero@users.noreply.github.com>
2024-05-06 22:42:50 +0700

    Update client.c

--- soal_4/client/client.c
+++ soal_4/client/client.c
@@ -1,1 +1,59 @@
+// client.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
+#define PORT 8080
+#define MAX_BUFFER_SIZE 1024
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[MAX_BUFFER_SIZE] = {0};
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server.\n");
+
+    while (1) {
+        printf("Enter command: ");
+        fgets(buffer, MAX_BUFFER_SIZE, stdin);
+
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        if (strcmp(buffer, "exit\n") == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+
+        memset(buffer, 0, sizeof(buffer));
+
+        recv(client_socket, buffer, MAX_BUFFER_SIZE, 0);
+        printf("Server response: %s\n", buffer);
+
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    close(client_socket);
+    return 0;
+}
