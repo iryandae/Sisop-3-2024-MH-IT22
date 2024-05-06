756a6d09952f25ee3fcc48fe234c1e78531564bf
Satsujinki99 <150358232+Satsujinki99@users.noreply.github.com>
2024-05-06 23:28:16 +0700

    Update action.c

--- soal_3/action.c
+++ soal_3/action.c
@@ -1,1 +1,45 @@
-//isi code di sini
+#include <stdio.h>
+#include <string.h>
+#include "actions.h"
+
+const char* check_gap(float gap) {
+    if (gap < 3.5) {
+        return "Gogogo";
+    } else if (gap <= 10) {
+        return "Push";
+    } else {
+        return "Stay out of trouble";
+    }
+}
+
+const char* check_fuel(float fuel) {
+    if (fuel > 80) {
+        return "Push Push Push";
+    } else if (fuel >= 50) {
+        return "You can go";
+    } else {
+        return "Conserve Fuel";
+    }
+}
+
+const char* check_tire(int tire) {
+    if (tire > 80) {
+        return "Go Push Go Push";
+    } else if (tire > 50) {
+        return "Good Tire Wear";
+    } else if (tire > 30) {
+        return "Conserve Your Tire";
+    } else {
+        return "Box Box Box";
+    }
+}
+
+const char* check_tire_change(const char* tire_type) {
+    if (strcmp(tire_type, "Soft") == 0) {
+        return "Mediums Ready";
+    } else if (strcmp(tire_type, "Medium") == 0) {
+        return "Box for Softs";
+    } else {
+        return "Invalid tire type";
+    }
+}
